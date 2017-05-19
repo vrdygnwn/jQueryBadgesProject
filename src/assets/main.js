cccbf8c431073a31d691559b10754d19e56b18fe
vrdygnwn <vrdygnwn@gmail.com>
2017-05-19 14:09:55 +0700

    adding the task

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,20 @@
 $(function() {
 
   // your code will go here
-
+  $.ajax({
+    url: 'https://www.codeschool.com/users/3621943.json',
+    dataType: 'jsonp',
+    success: function(response){
+      console.log(response.courses.completed)
+      $.each(response.courses.completed, function(index, property){
+        let title = '<h3>'+response.courses.completed[index].title+'</h3>';
+        let urlimg = response.courses.completed[index].badge;
+        let image = '<img src='+urlimg+'></img>';
+        let urlbtn = response.courses.completed[index].url;
+        let linkbtn = '<a class="contents" href='+urlbtn+' target="_blank">See Course</a>';
+        let btn = '<button class="MENU">'+linkbtn+'</button>';
+        $('#badges').append('<div class="course">'+title+image+btn+'</div>')
+      })
+    }
+  })
 });
